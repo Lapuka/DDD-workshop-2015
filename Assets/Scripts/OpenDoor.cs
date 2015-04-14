f7980c1cb544dd922edd36a68b75caf876efc960
Lapuka <lapukacom@gmail.com>
2015-04-14 13:38:16 +0200

    First Import

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+    Vector3 startPos;
+    void Start() {
+
+        startPos = transform.position;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+       
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y - 50f, transform.position.z), Time.deltaTime * 10);
+        StartCoroutine("Close");
+    }
+
+    IEnumerator Close()
+    {
+        Debug.Log("boompedS");
+        yield return new WaitForSeconds(5f);
+
+        transform.position = startPos;
+
+
+    }
+}
